3cf784becff71d1ce5a359fac924321fe9b40c38
SukirtiSaxena <sukirtisaxena@gmail.com>
2022-01-25 20:59:08 +0000

    Logic for Deuce, Advantage and Winner in Game

--- src/App.js
+++ src/App.js
@@ -8,17 +8,23 @@ function App() {
   const [scorePlayerOne, setScorePlayerOne] = useState(0);
   const [scorePlayerTwo, setScorePlayerTwo] = useState(0);
 
-  const [gameScore, setGameScore] = useState('The game is about to begin!!');
+  const [gameScore, setGameScore] = useState('The game is about to begin!! Its love-all ');
 
-  const incrementScoreP1 = () => { setScorePlayerOne(scorePlayerOne + 1); gameMessage(scorePlayerOne + 1,scorePlayerTwo); };
-  const incrementScoreP2 = () => { setScorePlayerTwo(scorePlayerTwo + 1); gameMessage(scorePlayerOne,scorePlayerTwo + 1); };
+  const incrementScoreP1 = () => { setScorePlayerOne(scorePlayerOne + 1); gameMessage(scorePlayerOne + 1, scorePlayerTwo); };
+  const incrementScoreP2 = () => { setScorePlayerTwo(scorePlayerTwo + 1); gameMessage(scorePlayerOne, scorePlayerTwo + 1); };
 
-  const gameMessage = (s1,s2) => {
-    if (s1 > s2)
-      setGameScore('Player One is the Winner');
-    if (s2 > s1)
-      setGameScore('Player Two is the Winner');
-    if (s1 === s2)
+  const gameMessage = (s1, s2) => {
+    if (s1 > 3 || s2 > 3) {
+      if (s1 === s2 + 1)
+        setGameScore('Player One is at Advantage');
+      if (s2 === s1 + 1)
+        setGameScore('Player Two is at Advantage');
+      if (s1 >= s2 + 2)
+        setGameScore('Player One is the Winner');
+      if (s2 >= s1 + 2)
+        setGameScore('Player Two is the Winner');
+    }
+    if (s1 > 3 && s1 === s2)
       setGameScore('Deuce');
   };
 
